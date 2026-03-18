5d27e9a3b248c397f3bf03c613b0b167a5d1132e
Sanjay <sanjay.selvaraj@deakin.edu.au>
2026-03-18 18:18:51 +1100

    Week 3 Photon Fusion

---
+++ Sanjay's - Do not touch/Week3 Photon Fusion/Week3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Fusion;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [SerializeField] private CharacterController ch;
+    public float playerSpeed = 5f;
+
+
+    public override void FixedUpdateNetwork()
+    {
+        if(HasStateAuthority == false)
+        {
+            return;
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput) * playerSpeed * Runner.DeltaTime;
+
+        ch.Move(movement);
+
+        if(movement!= Vector3.zero)
+        {
+            transform.forward = movement;
+        }
+    }
+}
