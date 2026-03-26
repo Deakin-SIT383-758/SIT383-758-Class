08735cbf13778481307f50666fd9652174d36ce1
Mick M <mickwpmcdonnell@gmail.com>
2026-03-26 11:08:53 +1030

    Implement saving of keypoint positions

--- MickM/Week4/Assets/Scripts/KeypointMatcher.cs
+++ MickM/Week4/Assets/Scripts/KeypointMatcher.cs
@@ -7,16 +7,29 @@ public class KeypointMatcher : MonoBehaviour
     public bool checkPinch = true;
     private HandDetection handDetection;
     [SerializeField] private float pinchThreshold = 0.15f;
+    [SerializeField] private List<Vector3[]> savedJointPositions;
 
     private void Awake()
     {
         handDetection = GetComponent<HandDetection>();
+        savedJointPositions = new List<Vector3[]>();
     }
 
+    private bool recordPositionsFlag = false;
+    private void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space))
+        {
+            recordPositionsFlag = true;
+        }
+    }
 
     bool isPinching = false;
     private void HandDetection_FrameDetectionCompleteEvent(Vector3[] jointPositions)
     {
+        if (recordPositionsFlag)
+            RecordPositions(jointPositions);
+
         if (checkPinch)
             CheckPinch(jointPositions);
         else
@@ -28,6 +41,14 @@ public class KeypointMatcher : MonoBehaviour
         string s = isPinching ? "Pinching" : "Not pinching";
         GUI.Label(new Rect(10, 10, 300, 200), s);
 
+    }
+
+    private void RecordPositions(Vector3[] jointPositions)
+    {
+        recordPositionsFlag = false;
+        savedJointPositions.Add(jointPositions);
+        lastSavedPositions = jointPositions;
+        Debug.Log($"Saved joint positions: {savedJointPositions.Count}. Positions:\n{savedJointPositions}");
     }
 
     void CheckPinch(Vector3[] jointPositions)
