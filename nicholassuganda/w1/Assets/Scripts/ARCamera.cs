1d3b8b482a085d8f766e617642251bf8bbcbdb64
Nicholas Suganda <sugandanicholas@gmail.com>
2026-03-26 13:51:21 +1100

    w4 initial commit

--- nicholassuganda/w1/Assets/Scripts/ARCamera.cs
+++ nicholassuganda/w1/Assets/Scripts/ARCamera.cs
@@ -1,21 +1,65 @@
 using UnityEngine;
+using TMPro;
 
 public class ARCamera : MonoBehaviour
 {
     public Material cubeMaterial;
-
     private WebCamTexture wcTexture;
+
+    public TextMeshProUGUI statusText;
+    public TextMeshProUGUI cameraText;
+    public TextMeshProUGUI photoText;
+
+    private WebCamDevice[] devices;
+    private int currentCameraIndex = 0;
+
     void Start()
     {
-        wcTexture = new WebCamTexture();
-
-        cubeMaterial.mainTexture = wcTexture;
-        wcTexture.Play();
+        devices = WebCamTexture.devices;
+        StartCamera();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void StartCamera()
+    {
+        if (wcTexture != null)
+        wcTexture.Stop();
+
+        wcTexture = new WebCamTexture(devices[currentCameraIndex].name);
+        cubeMaterial.mainTexture = wcTexture;
+        wcTexture.Play();
+
+        cameraText.text = "Using: " + devices[currentCameraIndex].name;
+
+        if (WebCamTexture.devices.Length == 0)
+        {
+            statusText.text = "No camera found!";
+        } else
+        {
+            statusText.text = "Good";
+        }
+    }
+    
+    public void TakePhoto()
+    {
+        string fileName = "photo_" + System.DateTime.Now.Ticks + ".png";
+        ScreenCapture.CaptureScreenshot(fileName);
+
+        photoText.text = "Saved: " + fileName;
+    }
+
+    public void SwitchFrontCamera()
+    {
+        currentCameraIndex = (currentCameraIndex + 1) % devices.Length;
+        StartCamera();
+    }
+    public void SwitchBackCamera()
+    {
+        currentCameraIndex = (currentCameraIndex - 1) % devices.Length;
+        StartCamera();
     }
 }
