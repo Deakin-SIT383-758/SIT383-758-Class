0f0ec7127803987de448543fd2527b220fbcb10d
LordofSpirals <dazzacopley@gmail.com>
2026-03-27 15:04:21 +1100

    Week 3 - name tag jitter fix
    Changed from Update to LateUpdate function

--- DarrenC/SIT383_Week3/Assets/Scripts/FaceCamera.cs
+++ DarrenC/SIT383_Week3/Assets/Scripts/FaceCamera.cs
@@ -11,7 +11,7 @@ public class FaceCamera : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.rotation = camTrans.rotation; // match camera rotation
     }
