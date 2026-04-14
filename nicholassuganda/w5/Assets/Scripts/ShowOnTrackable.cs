a5e64a768a7aae60115da6060d1461f2178303ed
Nicholas Suganda <sugandanicholas@gmail.com>
2026-04-14 18:12:05 +1000

    initial w5 commit

---
+++ nicholassuganda/w5/Assets/Scripts/ShowOnTrackable.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
+
+public class ShowOnTrackable : MonoBehaviour
+{
+    public List<GameObject> ObjectToPlace;
+    private ARTrackedImageManager arTrackedManager;
+    private Dictionary<string, GameObject> trackedObjects;
+
+    private void Awake()
+    {
+        arTrackedManager = GetComponent<ARTrackedImageManager>();
+    }
+
+    private void OnEnable()
+    {
+        arTrackedManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnDisable()
+    {
+        arTrackedManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    void Start()
+    {
+        trackedObjects = new Dictionary<string, GameObject>();
+        for(int i=0; i<arTrackedManager.referenceLibrary.count; i++)
+        {
+            GameObject go = Instantiate(ObjectToPlace[i]);
+            go.SetActive(false);
+            trackedObjects[arTrackedManager.referenceLibrary[i].name] = go;
+        }
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var addedImage in args.added)
+        {
+            trackedObjects[addedImage.referenceImage.name].SetActive(true);
+        }
+
+        foreach(var updatedImages in args.updated)
+        {
+            trackedObjects[updatedImages.referenceImage.name].SetActive(true);
+            trackedObjects[updatedImages.referenceImage.name].transform.position = updatedImages.transform.position;
+            trackedObjects[updatedImages.referenceImage.name].transform.rotation = updatedImages.transform.rotation;
+        }
+
+        foreach(var removeImage in args.removed)
+        {
+            trackedObjects[removeImage.referenceImage.name].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
