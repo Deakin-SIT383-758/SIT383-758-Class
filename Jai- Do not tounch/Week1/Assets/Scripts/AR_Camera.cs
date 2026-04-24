58ee4d4062a7094f1d4de378a64778fea5ea94a8
jaiwats <jaiwats@gmail.com>
2026-04-24 13:11:07 +1000

    Week1CommitedJai

--- Jai- Do not tounch/Week1/Assets/Scripts/AR_Camera.cs
+++ Jai- Do not tounch/Week1/Assets/Scripts/AR_Camera.cs
@@ -1,19 +1,52 @@
 using UnityEngine;
+using TMPro;
 
 public class AR_Camera : MonoBehaviour
 {
     public Material cubeMaterail;
     private WebCamTexture WCTexture;
+    private int currentCamIndex = 0;
+    private WebCamDevice[] devices;
+    public TextMeshProUGUI ButtonText;
+    public TextMeshProUGUI SpecialText;
+    private string allDevices;
     void Start()
     {
-        WCTexture = new WebCamTexture();
-        cubeMaterail.mainTexture = WCTexture;
-        WCTexture.Play();
+        devices = WebCamTexture.devices;
+        if (devices.Length > 0)
+        {
+            ButtonText.SetText(devices[currentCamIndex].name);
+            PlayCamera(currentCamIndex);
+
+            for (int i = 0; i < devices.Length;)
+            {
+                allDevices += devices[i].name + ", ";
+                i++;
+            }
+            SpecialText.SetText(allDevices);
+        }
     }
 
-
-    void Update()
+    public void SwitchCamera()
     {
+        if (devices.Length == 0) return;
 
+        currentCamIndex = (currentCamIndex + 1) % devices.Length;
+        ButtonText.SetText(devices[currentCamIndex].name);
+        PlayCamera(currentCamIndex);
+    }
+
+    void PlayCamera(int index)
+    {
+        if (WCTexture != null)
+        {
+            WCTexture.Stop();
+        }
+
+        WebCamDevice device = devices[index];
+        WCTexture = new WebCamTexture(device.name);
+        ButtonText.SetText(device.name);
+        cubeMaterail.mainTexture = WCTexture;
+        WCTexture.Play();
     }
 }
